a58dd07c7744a333982539d7692bd4730b3e9862
Josh Huffman and Nhat Hoang <josh@>
2016-08-23 16:04:27 -0700

    add background, styling

--- js/scripts.js
+++ js/scripts.js
@@ -28,50 +28,70 @@ function findAccount(name) {
   });
   return accountTemp;
 }
+
+function printReceipt(account) {
+  $("#receipt").text("");
+  $("#receipt").append("<p>Thanks for banking with us today " + account.firstName + "!</p>" +
+                        "<p>Your account balance is $" + account.deposit + "</p>");
+}
+
 function resetInput() {
   $("input.firstname-input").val("");
   $("input.lastname-input").val("");
   $("input.deposit-initial").val("");
   $("input.withdraw-input").val("");
   $("input.deposit-input").val("");
+  $("input.acct-deposit-input").val("");
+  $("input.acct-withdraw-input").val("");
 }
 
 $(document).ready(function() {
-
   $("#createAccount").submit(function(event){
     event.preventDefault();
     var firstName = $("input.firstname-input").val();
     var lastName = $("input.lastname-input").val();
-    var deposit = parseInt($("input.deposit-initial").val());
+    var deposit = parseFloat($("input.deposit-initial").val());
 
     account = new Name(firstName, lastName, deposit);
     accountArray.push(account);
     accountIndex.push(firstName);
     console.log(account);
+    printReceipt(account);
     resetInput();
   });
 
   $("button.deposit-input").click(function(){
-    var accountName = $("input.firstname-input").val();
+    var accountName = $("input.acct-deposit-input").val();
     if (findAccount(accountName) !== -1){
       account = findAccount(accountName);
-      account.addMoney(parseInt($("input.deposit-input").val()));
+      account.addMoney(parseFloat($("input.deposit-input").val()));
+      printReceipt(account);
       console.log(account);
     } else {
+      $("#receipt").text("");
       alert("account has not been created");
-    }  resetInput();
+    }
+
+    resetInput();
 
   });
 
   $("button.withdraw-input").click(function(){
-    var accountName = $("input.firstname-input").val();
+    var accountName = $("input.acct-withdraw-input").val();
     if (findAccount(accountName) !== -1){
       account = findAccount(accountName);
-      account.subtractMoney(parseInt($("input.withdraw-input").val()));
+      var withdrawHolder = parseFloat($("input.withdraw-input").val());
+      if (withdrawHolder > account.deposit) {
+        alert("You have unsufficient fund!");
+      }else account.subtractMoney(parseFloat($("input.withdraw-input").val()));
+      printReceipt(account);
       console.log(account);
     } else {
+      $("#receipt").text("");
       alert("account has not been created");
-    }  resetInput();
+    }
+
+    resetInput();
   });
 
 });
