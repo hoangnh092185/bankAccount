816fa6e20304c5c8dfd4c139c778e78f9997c8b5
Josh Huffman and Nhat Hoang <josh@>
2016-08-23 14:24:29 -0700

    verify the input name to the array to see if account has been created. If it is true, deposit or withdraw money from specific account identity.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 
+var account;
+var accountArray = [];
+var accountIndex = [];
+
+
 function Name(first, last, deposit){
   this.firstName = first;
   this.lastName = last;
@@ -14,15 +19,59 @@ Name.prototype.subtractMoney = function(amount){
   return this.deposit;
 }
 
+function findAccount(name) {
+  var accountTemp = -1;
+  accountIndex.forEach(function(element, index){
+    if (element === name) {
+      accountTemp = accountArray[index];
+    }
+  });
+  return accountTemp;
+}
+function resetInput() {
+  $("input.firstname-input").val("");
+  $("input.lastname-input").val("");
+  $("input.deposit-initial").val("");
+  $("input.withdraw-input").val("");
+  $("input.deposit-input").val("");
+}
+
 $(document).ready(function() {
+
   $("#createAccount").submit(function(event){
     event.preventDefault();
     var firstName = $("input.firstname-input").val();
     var lastName = $("input.lastname-input").val();
-    var deposit = parseInt($("input.deposit-input").val());
-    var account = new Name(firstName, lastName, deposit);
+    var deposit = parseInt($("input.deposit-initial").val());
+
+    account = new Name(firstName, lastName, deposit);
+    accountArray.push(account);
+    accountIndex.push(firstName);
     console.log(account);
-    debugger;
+    resetInput();
+  });
+
+  $("button.deposit-input").click(function(){
+    var accountName = $("input.firstname-input").val();
+    if (findAccount(accountName) !== -1){
+      account = findAccount(accountName);
+      account.addMoney(parseInt($("input.deposit-input").val()));
+      console.log(account);
+    } else {
+      alert("account has not been created");
+    }  resetInput();
+
+  });
+
+  $("button.withdraw-input").click(function(){
+    var accountName = $("input.firstname-input").val();
+    if (findAccount(accountName) !== -1){
+      account = findAccount(accountName);
+      account.subtractMoney(parseInt($("input.withdraw-input").val()));
+      console.log(account);
+    } else {
+      alert("account has not been created");
+    }  resetInput();
   });
 
 });
